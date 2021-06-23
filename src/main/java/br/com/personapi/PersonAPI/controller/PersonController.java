d451b9394243737f3f2927f67cab9a6869dbb258
Gustavo Weiss <gustavoweissrocha@gmail.com>
2021-06-22 22:20:09 -0300

    Better Constructing Made

--- src/main/java/br/com/personapi/PersonAPI/controller/PersonController.java
+++ src/main/java/br/com/personapi/PersonAPI/controller/PersonController.java
@@ -4,6 +4,7 @@ import br.com.personapi.PersonAPI.dto.response.MessageResponseDTO;
 import br.com.personapi.PersonAPI.dto.request.PersonDTO;
 import br.com.personapi.PersonAPI.exception.PersonNotFoundException;
 import br.com.personapi.PersonAPI.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private final PersonService personService;
 
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
